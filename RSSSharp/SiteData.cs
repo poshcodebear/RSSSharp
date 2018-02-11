7f82033c71a661ad328b04637249849c1a8103a6
The Powershell Bear <poshcodebear@outlook.com>
2018-02-10 22:05:09 -0800

    Added base project
    Included classes I think I'll use

---
+++ RSSSharp/SiteData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSSharp
+{
+    class SiteData
+    {
+        List<Post> posts;
+        Website website;
+        DateTime lastChecked;
+    }
+}
---
+++ RSSSharp/Website.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSSharp
+{
+    class Website
+    {
+        Feed feed;
+        string url;
+
+        public Post GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
